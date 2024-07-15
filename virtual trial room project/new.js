6fad3011897eea6bd1380a0629c7b1b3113080b4
Sansidhi Shukla <159580610+Sansidhi-Shukla@users.noreply.github.com>
2024-07-16 01:01:48 +0530

    Add files via upload

---
+++ virtual trial room project/new.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tryOnButton = document.getElementById('try-on-button');
+    const trialRoom = document.getElementById('trial-room');
+    const nextButton = document.getElementById('next-button');
+    const modelViewer = document.getElementById('model-viewer');
+
+    tryOnButton.addEventListener('click', function () {
+        trialRoom.classList.remove('hidden');
+    });
+
+    
+});
